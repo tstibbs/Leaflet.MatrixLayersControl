89dc281f0c0b6e7ad74bf78c02f1e45dfcb27ccd
tstibbs <tstibbs@users.noreply.github.com>
2016-07-24 22:10:13 +0100

    Moved markers

--- examples/simple.js
+++ examples/simple.js
@@ -1,18 +1,25 @@
-var cities = new L.LayerGroup();
+var urbanParks = new L.LayerGroup();
+var urbanBuildings = new L.LayerGroup();
+var riversideParks = new L.LayerGroup();
+var riversideBuildings = new L.LayerGroup();
 
-L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.').addTo(cities),
-L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.').addTo(cities),
-L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(cities),
-L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(cities);
+L.marker([51.503984, -0.118253]).addTo(riversideParks);
+L.marker([51.497466, -0.124905]).addTo(riversideParks);
+L.marker([51.506061, -0.116966]).addTo(riversideBuildings);
+L.marker([51.499102, -0.124648]).addTo(riversideBuildings);
+L.marker([51.503797, -0.143649]).addTo(urbanParks);
+L.marker([51.530535, -0.153362]).addTo(urbanParks);
+L.marker([51.500784, -0.143052]).addTo(urbanBuildings);
+L.marker([51.497770, -0.101477]).addTo(urbanBuildings);
 
 var streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 
 var map = L.map('map', {
-	center: [39.73, -104.99],
-	zoom: 10,
-	layers: [streets, cities]
+	center: [51.515, -0.13],
+	zoom: 13,
+	layers: [streets, urbanParks, urbanBuildings, riversideParks, riversideBuildings]
 });
 
 var baseLayers = {
@@ -20,7 +27,10 @@ var baseLayers = {
 };
 
 var overlays = {
-	"Cities": cities
+	"urbanParks": urbanParks,
+	"urbanBuildings": urbanBuildings,
+	"riversideParks": riversideParks,
+	"riversideBuildings": riversideBuildings
 };
 
 L.control.layers(baseLayers, overlays).addTo(map);
